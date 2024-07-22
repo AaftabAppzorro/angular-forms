afab62001bb5607b70065c2e78b796ba807be9dc
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-07-22 22:25:57 +0530

    Fully committed at once.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from "./auth/signup/signup.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    imports: [LoginComponent, SignupComponent]
 })
-export class AppComponent {
-  title = 'angular-forms';
-}
+export class AppComponent {}
